32af09dcd25409c4fc4e61c5c022f8105d1ff13c
Filipi Mosquini <filipimosquini@gmail.com>
2024-08-28 20:10:08 -0300

    Resolve a questão 1

---
+++ src/Questao1/ContaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Questao1
+{
+    public class ContaBancaria {
+
+        public int NumeroConta { get; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroConta, string nome, double depositoInicial = 0.00)
+        {
+            Nome = nome;
+            NumeroConta = numeroConta;
+            Saldo = depositoInicial;
+        }
+
+        public void Deposito(double quantia)
+        {
+            if (quantia < 0)
+            {
+                throw new ArgumentException("O valor não pode ser negativo");
+            }
+
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            const double taxa = 3.50;
+
+            if (quantia < 0)
+            {
+                throw new ArgumentException("O valor não pode ser negativo");
+            }
+
+            Saldo -= quantia;
+            Saldo -= taxa;
+        }
+
+        public override string ToString()
+            => $"Conta {NumeroConta}, Titular: {Nome}, Saldo: $ {string.Format("{0:C}", Saldo)}";
+    }
+}
