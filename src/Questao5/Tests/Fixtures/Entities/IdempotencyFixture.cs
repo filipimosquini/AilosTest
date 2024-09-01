1a21d37919a9a136bee652743ad02203c9f6d7f1
Filipi Mosquini <filipimosquini@gmail.com>
2024-09-01 12:44:22 -0300

    Questão 5 - Cria os testes unitários para o service idepotência

---
+++ src/Questao5/Tests/Fixtures/Entities/IdempotencyFixture.cs
@@ -0,0 +1,31 @@
+﻿using Questao5.Domain.Entities;
+using System;
+
+namespace Questao5.Tests.Fixtures.Entities;
+
+public class IdempotencyFixture : IDisposable
+{
+    public IdempotencyFixture() { }
+
+    public Idempotency Generate(bool generateRandomId = false)
+    {
+        string _id = "25561A63-FE01-41FB-BB97-87E4A9B64AC1";
+
+        if (generateRandomId)
+        {
+            _id = Guid.NewGuid().ToString();
+        }
+
+        return new Idempotency
+        {
+            Id = _id,
+            Request = "\"{\\r\\n  'requestId': '25561a63-fe01-41fb-bb97-87e4a9b64ac1',\\r\\n  'accountNumber': 123,\\r\\n  'amount': 300,\\r\\n  'movementType': 'C'\\r\\n}\"",
+            Response = "\"{'data':{'movementId':'5665e52e-89b1-40fc-8984-3320b0803a8a'}}\""
+        };
+    }
+
+    public Idempotency IdempotencyNull
+        => null;
+
+    public void Dispose() { }
+}
---
+++ src/Questao5/Tests/Infrastructure/Services/Idempotencies/IdempotencyServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using Questao5.Domain.Stores;
+using Questao5.Infrastructure.Database.Idempotencies.Models;
+using Questao5.Infrastructure.Services.Idempotencies;
+using Questao5.Tests.Fixtures.Entities;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Questao5.Tests.Infrastructure.Services.Idempotencies;
+
+[Trait("Service Tests", "IdempotencyService")]
+public class IdempotencyServiceTests : IClassFixture<IdempotencyFixture>
+{
+    private readonly IdempotencyFixture _idempotencyFixture;
+
+    private readonly IIdempotencyQueryStore _idempotencyQueryStore;
+    private readonly IIdempotencyCommandStore _idempotencyCommandStore;
+
+    private IdempotencyService _idempotencyService;
+
+    public IdempotencyServiceTests(IdempotencyFixture idempotencyFixture)
+    {
+        _idempotencyFixture = idempotencyFixture;
+        _idempotencyQueryStore = Substitute.For<IIdempotencyQueryStore>();
+        _idempotencyCommandStore = Substitute.For<IIdempotencyCommandStore>();
+    }
+
+    [Fact(DisplayName = "CreateIdempotencyAsyncShouldNotThrowException")]
+    public async Task CreateIdempotencyAsyncShouldNotThrowException()
+    {
+        // Arrange 
+
+        var idempotency = _idempotencyFixture.Generate();
+
+        _idempotencyCommandStore.AddIdepotencyAsync(new CreateIdepotencyRequest
+        {
+            Request = idempotency.Request,
+            Id = Guid.Parse(idempotency.Id)
+        });
+
+        _idempotencyService = new IdempotencyService(_idempotencyQueryStore, _idempotencyCommandStore);
+
+        // Act
+
+        var result = await _idempotencyService.CreateIdepotencyAsync(Guid.Parse(idempotency.Id), idempotency.Request);
+
+        // Assert
+
+        result.Should().NotBeNull();
+    }
+
+    [Fact(DisplayName = "UpdateIdepotencyAsyncShouldNotThrowException")]
+    public async Task UpdateIdepotencyAsyncShouldNotThrowException()
+    {
+        // Arrange 
+
+        var idempotency = _idempotencyFixture.Generate();
+
+        _idempotencyCommandStore.UpdateIdepotencyResponse(new UpdateIdepotencyRequest
+        {
+            Response = idempotency.Response,
+            Id = Guid.Parse(idempotency.Id)
+        });
+
+        _idempotencyService = new IdempotencyService(_idempotencyQueryStore, _idempotencyCommandStore);
+
+        // Act
+
+        var act = async ()  => await _idempotencyService.UpdateIdepotencyAsync(Guid.Parse(idempotency.Id), idempotency.Response);
+
+        // Assert
+
+        await act.Should().NotThrowAsync();
+    }
+}
